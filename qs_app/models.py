af8ffb47458aa66626d1114a42834ef60af89266
Megan Arellano <ssciolist@gmail.com>
2018-07-05 19:33:42 -0600

    changes Meal to have a many to many field and deletes MealFood model. Runs migrations for models

--- qs_app/models.py
+++ qs_app/models.py
@@ -7,7 +7,4 @@ class Food(models.Model):
 
 class Meal(models.Model):
     name = models.CharField(max_length=200)
-
-class MealFood(models.Model):
-    meal_id: models.ForeignKey(Meal, on_delete=models.CASCADE)
-    food_id: models.ForeignKey(Food, on_delete=models.CASCADE)
+    foods = models.ManyToManyField(Food)

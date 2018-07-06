eb485a19ac90f7a86a1acec2313431b43d20ff4e
Megan Arellano <ssciolist@gmail.com>
2018-07-05 19:22:18 -0600

    makes models

--- qs_app/models.py
+++ qs_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Food(models.Model):
+    name = models.CharField(max_length=200)
+    calories = models.IntegerField()
+
+class Meal(models.Model):
+    name = models.CharField(max_length=200)
+
+class MealFood(models.Model):
+    meal_id: models.ForeignKey(Meal, on_delete=models.CASCADE)
+    food_id: models.ForeignKey(Food, on_delete=models.CASCADE)

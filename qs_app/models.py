f791720faf3db73e6496407b44c5d2e0f963a6ed
Megan Arellano <ssciolist@gmail.com>
2018-07-06 07:38:36 -0600

    adds string method to models

--- qs_app/models.py
+++ qs_app/models.py
@@ -4,7 +4,11 @@ from django.db import models
 class Food(models.Model):
     name = models.CharField(max_length=200)
     calories = models.IntegerField()
+    def __str__(self):
+        return self.name
 
 class Meal(models.Model):
     name = models.CharField(max_length=200)
     foods = models.ManyToManyField(Food)
+    def __str__(self):
+        return self.name

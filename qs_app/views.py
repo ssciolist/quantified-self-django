2bd80c2dece62d28e57c136ffd8c82a5cb8418fb
Megan Arellano <ssciolist@gmail.com>
2018-07-07 15:19:56 -0600

    writes implementation for foods show

--- qs_app/views.py
+++ qs_app/views.py
@@ -4,9 +4,14 @@ from django.http import HttpResponse
 # imports for jsonification
 from django.http import JsonResponse
 from django.core import serializers
+from django.forms.models import model_to_dict
 
 from qs_app.models import Food
 
 def food_index(request):
     foods = list(Food.objects.all().values('id', 'name', 'calories'))
     return JsonResponse(foods, safe=False)
+
+def food_show(request, food_id):
+    food = model_to_dict(Food.objects.get(pk=food_id))
+    return JsonResponse(food, safe=False)

6da29df5b12721aa855fbb22f1b6fdade649170d
Megan Arellano <ssciolist@gmail.com>
2018-07-07 19:47:48 -0600

    functional post method for foods. Happy path only

--- qs_app/views.py
+++ qs_app/views.py
@@ -5,13 +5,28 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 from django.core import serializers
 from django.forms.models import model_to_dict
+import json
+
+# import to allow some requests without CSRF protection
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
 
 from qs_app.models import Food
 
+@csrf_exempt
 def food_index(request):
-    foods = list(Food.objects.all().values('id', 'name', 'calories'))
-    return JsonResponse(foods, safe=False)
+    if request.method == 'GET':
+        foods = list(Food.objects.all().values('id', 'name', 'calories'))
+        return JsonResponse(foods, safe=False)
+    elif request.method == 'POST':
+        food_params = json.loads(request.body)['food']
+        food = Food(name = food_params['name'], calories = food_params['calories'])
+        food.save()
+        return JsonResponse({'id':food.id, 'name':food.name, 'calories':food.calories})
+
 
 def food_show(request, food_id):
-    food = model_to_dict(Food.objects.get(pk=food_id))
-    return JsonResponse(food, safe=False)
+    if request.method == 'GET':
+        food = model_to_dict(Food.objects.get(pk=food_id))
+        return JsonResponse(food, safe=False)
+    elif request.method == 'POST':
+        return HttpResponse('placeholder')
